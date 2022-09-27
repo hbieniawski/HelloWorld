be16797a02be10d8711a8760a515acc47167e6e5
hbieniawski <113638047+hbieniawski@users.noreply.github.com>
2022-09-27 13:21:10 -0700

    Add files via upload

---
+++ helloworld/src/App.java
@@ -0,0 +1,7 @@
+public class App  {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Pikachu welcomes you to the world of Pokémon!\n\n(\\__/)\n(o^.^)\nz(_('')('')") ;
+
+
+    }
+}
